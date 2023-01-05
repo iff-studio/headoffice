e9a4cf58faf463f235d3aa0fbe62dde510f4a92e
maciekpaprocki <maciekpaprocki@gmail.com>
2023-01-05 16:25:15 +0000

    added fix for sizes

--- components/ModelSizes.js
+++ components/ModelSizes.js
@@ -3,7 +3,7 @@ export default function ModelSizes ({ model, className }) {
         {
             label: 'Shoe',
             key: 'shoe',
-            sign: ''
+            sign: ' UK'
         },
         {
             label: 'Waist',
@@ -25,17 +25,36 @@ export default function ModelSizes ({ model, className }) {
             sign: '″',
             type: 'inch',
         },
-    ]
-    return <div className={className + ' clearfix'}>
-        <div className='float-left w-6/12'>{model.heightFeet && `Height: ${model.heightFeet}'${model.heightInches || ''}`}</div>
+    ];
+    let heightText = '';
+    let heightCM = 0;
+    if(model.heightFeet){
+        heightCM = model.heightFeet * 30.48;
+        if(model.heightInches){
+            heightCM += model.heightInches * 2.54;
+        }
+        if(heightCM){
+            heightText = `Height: ${model.heightFeet}'${model.heightInches?(model.heightInches+'″'):''} / ${Math.floor(heightCM)}cm`
+        }
+
+    }
+
+    {model.heightFeet && `Height: ${model.heightFeet}'${model.heightInches || ''}`}
+    return <div className={className + ' clearfix text-xs'}>
+        <div
+            className="float-left w-6/12">{heightText}</div>
         {measurements.map((measurement, key) => {
             let val = model[measurement.key]
             if (!val) {
                 return null
             }
-            return <div className='float-left w-6/12' key={key}>
-                {measurement.label}: {val}{measurement.sign}
 
+            let text = `${measurement.label}: ${val}${measurement.sign}`
+            if (measurement.type === 'inch') {
+                text += ` / ${Math.ceil(val * 2.54)}cm`
+            }
+            return <div className="float-left w-6/12" key={key}>
+                {text}
             </div>
 
         })}
