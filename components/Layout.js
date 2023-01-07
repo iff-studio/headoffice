fefe441cf46ff7585658f39aa567b9bf952b0b89
maciekpaprocki <maciekpaprocki@gmail.com>
2023-01-07 23:13:17 +0000

    moved preview up

--- components/Layout.js
+++ components/Layout.js
@@ -8,7 +8,7 @@ export default function Layout ({ preview, children }) {
         <>
             <Meta/>
             <div className="min-h-screen">
-                <PreviewAlert preview={preview}/>
+
                 <div className="pt-[3.25rem]"></div>
                 <nav
                     className="border-black z-10 border-b border-solid font-bold tracking-tighter leading-tight fixed top-0 left-0 right-0 bg-white">
@@ -19,6 +19,7 @@ export default function Layout ({ preview, children }) {
                         <li className={'pr-3 inline'}><Link href={'/contact'}>Contact</Link></li>
                     </ul>
                 </nav>
+                <PreviewAlert preview={preview}/>
                 <main>{children}</main>
 
                 <Footer/>
