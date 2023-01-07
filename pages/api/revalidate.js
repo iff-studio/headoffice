d39b81bfe68914886b7e796fb0965cb9158fe8b7
maciekpaprocki <maciekpaprocki@gmail.com>
2023-01-07 22:31:27 +0000

    added error to output

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -32,12 +32,10 @@ export default async function handler (req, res) {
             await res.revalidate(`/${postSlug}`)
         }
 
-
-
         return res.json({ revalidated: true })
     } catch (err) {
         // If there was an error, Next.js will continue
         // to show the last successfully generated page
-        return res.status(500).send('Error revalidating')
+        return res.status(500).send('Error revalidating: ' + err.toString())
     }
 }
