d12671730f4f9bc8f03930a073b4f5febd5d63dc
maciekpaprocki <maciekpaprocki@gmail.com>
2023-01-07 23:15:41 +0000

    fixed formatting

--- components/PreviewAlert.js
+++ components/PreviewAlert.js
@@ -1,16 +1,15 @@
 export default function PreviewAlert ({ preview }) {
     if (!preview) {
-        return null;
+        return null
     }
     return (
         <div className="bg-accent-7 border-accent-7 text-white">
             <div className="py-2 text-center text-sm">
-                This is page is a preview.
+                This is page is a preview.&nbsp;
                 <a href="/api/exit-preview"
                    className="underline hover:text-cyan duration-200 transition-colors">
-                    Click here
+                    Click here to exit preview mode.
                 </a>
-                to exit preview mode.
             </div>
         </div>
     )
