c3699aced38daaf9552d8c3aac14797f8d89037d
maciekpaprocki <maciekpaprocki@gmail.com>
2023-01-08 00:06:20 +0000

    added fix for preview

--- components/PreviewAlert.js
+++ components/PreviewAlert.js
@@ -6,7 +6,7 @@ export default function PreviewAlert ({ preview }) {
         <div className="bg-accent-7 border-accent-7 text-white">
             <div className="py-2 text-center text-sm">
                 This is page is a preview.&nbsp;
-                <a href="/api/exit-preview"
+                <a href={`/api/exit-preview?path=${window.location.pathname}`}
                    className="underline hover:text-cyan duration-200 transition-colors">
                     Click here to exit preview mode.
                 </a>
--- pages/api/exit-preview.js
+++ pages/api/exit-preview.js
@@ -1,8 +1,9 @@
-export default async function exit(_, res) {
+export default async function exit(req, res) {
+  const { path } = req.query
   // Exit the current user from "Preview Mode". This function accepts no args.
   res.clearPreviewData()
 
   // Redirect the user back to the index page.
-  res.writeHead(307, { Location: '/' })
+  res.writeHead(307, { Location: path })
   res.end()
 }
