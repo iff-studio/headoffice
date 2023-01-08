79a316e063762c34137fcf7741668168dbb3ccc4
maciekpaprocki <maciekpaprocki@gmail.com>
2023-01-08 00:22:34 +0000

    moved path to useffect

--- components/PreviewAlert.js
+++ components/PreviewAlert.js
@@ -1,12 +1,23 @@
+import { useEffect } from 'react'
+
 export default function PreviewAlert ({ preview }) {
+    let $a = null
+
     if (!preview) {
         return null
     }
+
+    useEffect(function () {
+        if (window) {
+            $a.setAttribute('href', `/api/exit-preview?path=${window ? window.location.pathname : '/'}`)
+        }
+    })
+
     return (
         <div className="bg-accent-7 border-accent-7 text-white">
             <div className="py-2 text-center text-sm">
                 This is page is a preview.&nbsp;
-                <a href={`/api/exit-preview?path=${window ? window.location.pathname : '/'}`}
+                <a ref={(el) => $a = el} href="/api/exit-preview"
                    className="underline hover:text-cyan duration-200 transition-colors">
                     Click here to exit preview mode.
                 </a>
