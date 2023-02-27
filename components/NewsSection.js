c46b3ed85255ed2e05301a6c2f60fd903c6a5c19
maciekpaprocki <maciekpaprocki@gmail.com>
2023-02-27 22:05:14 +0000

    added leading story

--- components/NewsSection.js
+++ components/NewsSection.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import DateComponent from './DateComponent'
 import SideTitle from './SideTitle'
 
-export default function NewsSection ({ news = [], title = 'News' }) {
+export default function NewsSection ({ news = [], title = 'News', hasLeading= true }) {
 
     if (!news.length) {
         return null
@@ -12,7 +12,8 @@ export default function NewsSection ({ news = [], title = 'News' }) {
     return <section className={'relative -mt-0.5 border border-white border-l-black border-t-black clearfix'}>
         <SideTitle>{title}</SideTitle>
         {news.map((post, key) => {
-            return <div key={post.slug} className="border border-fix border-black md:w-1/2 float-left">
+            return <div key={post.slug}
+                        className={`border border-fix border-black float-left ${key === 0 && hasLeading ? 'w-full' : 'md:w-1/2'}`}>
                 <CoverImage title={post.title} slug={post.slug} url={post.coverImage.url} alt=""/>
                 <div className="p-4 bg-white">
                     <h3 className="text-3xl mb-1 leading-snug">
