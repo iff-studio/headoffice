9e40058b4d2684d3ee2a626689df628a16b40e6b
maciekpaprocki <maciekpaprocki@gmail.com>
2023-02-28 00:57:26 +0000

    added fix for footer

--- components/Footer.js
+++ components/Footer.js
@@ -23,8 +23,7 @@ function validateEmail (email) {
 
 export default function Footer () {
 
-    const subscribeContext = useContext(SubscribeContext)
-    const [status, setStatus] = useState(subscribeContext.subscribed ? STATUS.HIDDEN : STATUS.INITIAL)
+    const [status, setStatus] = useState(STATUS.INITIAL)
 
     let $form = null
     let $email = null
@@ -46,9 +45,7 @@ export default function Footer () {
                 return
             }
             setStatus(STATUS.SUCCESS)
-            setTimeout(function () {
-                subscribeContext.setSubscribed(true)
-            }, 300)
+
         })
     }
 
@@ -80,35 +77,36 @@ export default function Footer () {
 
     return <footer id={'footer'}>
         <div className="pt-[3.125rem]"></div>
-        <div
-            className={`max-w-screen-2xl mx-auto border-black z-10 border-t-2 border-l-2 transition-margin border-solid tracking-tighter leading-tight fixed bottom-0 left-0 right-0 bg-white ${status === STATUS.SUCCESS ? '-mb-[3.25rem]' : ''}`}>
-            {displayInput && <form onSubmit={submit}
-                                   action="https://headofficemgmt.us14.list-manage.com/subscribe/post"
-                                   method="post"
-                                   ref={(form) => $form = form}
-                                   className="flex"
-                                   name="mc-embedded-subscribe-form"
-                                   target="_blank">
-                <input type="email"
-                       name="EMAIL"
-                       ref={(input) => $email = input}
-                       className="p-4 outline-none w-full"
-                       id="mce-EMAIL"
-                       placeholder="Subscribe to Head Office MGMT"/>
-                <input type="hidden" name="u" value="eb60cbb9850faa8e42621d903" readOnly={true}/>
-                <input type="hidden" name="id" value="a3b92b7385" readOnly={true}/>
-                <div style={{ position: 'absolute', left: '-5000px' }} aria-hidden="true">
-                    <input type="text" name="b_eb60cbb9850faa8e42621d903_a3b92b7385" readOnly={true} tabIndex="-1"
-                           value=""/>
-                </div>
-                {message && <div className={`flex-1 p-4 ${isError ? 'text-red' : ''}`}>{message}</div>}
-                <button className="py-4 px-12 bg-black text-white font-bold uppercase" onClick={submit}
-                        type="submit">Submit
-                </button>
+        <div className={'border-t-2 border-black z-10 fixed bottom-0 left-0 right-0 bg-white'}>
+            <div className={`max-w-screen-2xl mx-auto border-black z-10 border-l-2 transition-margin border-solid ${status === STATUS.SUCCESS ? '-mb-[3.25rem]' : ''}`}>
+                {displayInput && <form onSubmit={submit}
+                                       action="https://headofficemgmt.us14.list-manage.com/subscribe/post"
+                                       method="post"
+                                       ref={(form) => $form = form}
+                                       className="flex"
+                                       name="mc-embedded-subscribe-form"
+                                       target="_blank">
+                    <input type="email"
+                           name="EMAIL"
+                           ref={(input) => $email = input}
+                           className="p-4 outline-none w-full"
+                           id="mce-EMAIL"
+                           placeholder="Subscribe to Head Office MGMT"/>
+                    <input type="hidden" name="u" value="eb60cbb9850faa8e42621d903" readOnly={true}/>
+                    <input type="hidden" name="id" value="a3b92b7385" readOnly={true}/>
+                    <div style={{ position: 'absolute', left: '-5000px' }} aria-hidden="true">
+                        <input type="text" name="b_eb60cbb9850faa8e42621d903_a3b92b7385" readOnly={true} tabIndex="-1"
+                               value=""/>
+                    </div>
+                    {message && <div className={`flex-1 p-4 ${isError ? 'text-red' : ''}`}>{message}</div>}
+                    <button className="py-4 px-12 bg-black text-white font-bold uppercase" onClick={submit}
+                            type="submit">Submit
+                    </button>
 
-            </form>}
-            {!displayInput && <div className={`flex-1 p-4 ${isError ? 'text-red' : ''}`}>{message}</div>}
+                </form>}
+                {!displayInput && <div className={`flex-1 p-4 ${isError ? 'text-red' : ''}`}>{message}</div>}
 
+            </div>
         </div>
     </footer>
 
